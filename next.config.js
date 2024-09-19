eb7ecb8b2ecb54986985fd72228b16d836daad1c
elenalape <hi@elenalape.com>
2024-09-19 15:37:51 -0700

    add image path to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/hackathon-tpac-2024",
+  assetPrefix: "/hackathon-tpac-2024/",
+  output: "export",
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
